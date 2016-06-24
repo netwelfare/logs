cf894fe86627ec594d2475f2217ba26a8777aff9
wangxiaofei <wangxiaofei@corp.netease.com>
2016-06-24 20:14:59 +0800

    建立日志开源项目

---
+++ log4j/code/Test.java
@@ -0,0 +1,21 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		Log log = LogFactory.getLog(Test.class);
+		log.info("info");
+		/*float x = 1413514800 - 1413168789;
+		System.out.println(x);
+		// 4天多少秒
+		float day = 24 * 60 * 60 * 4;
+		System.out.println(day);
+		float cha = x - day;
+		System.out.println(cha);
+		System.out.println(System.currentTimeMillis());*/
+	}
+
+}
